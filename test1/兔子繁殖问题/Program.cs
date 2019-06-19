ce9244b40b8d09830da6d7584e00baa0bf5d28b3
KipKips <2432719656@qq.com>
2019-06-19 15:16:42 +0800

    兔子繁殖问题

---
+++ test1/兔子繁殖问题/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 兔子繁殖问题
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            int sum = 1;
+            for(int i = 1; i < 21; i++)
+            {
+               
+                if (i % 3 == 0)
+                {
+                    num *= 2;
+                    sum += num;
+                }
+            }
+            Console.WriteLine(sum*2);
+            Console.ReadKey();
+        }
+    }
+}
