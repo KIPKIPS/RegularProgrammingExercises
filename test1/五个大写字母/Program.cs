c8952b530473974575ae9a3fe4ef60f0bf255995
KipKips <2432719656@qq.com>
2019-06-19 16:18:27 +0800

    习题三

---
+++ test1/五个大写字母/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 五个大写字母
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("请输入五个大写字母：");
+            while (true)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    char word = Convert.ToChar(Console.ReadLine());
+                    if (word >= 'a' && word <= 'z')
+                    {
+                        Console.WriteLine("请输入大写字母");
+                    }
+                    if (count < 5)
+                    {
+                        if (word >= 'A' && word <= 'Z')
+                        {
+                            count++;
+                            Console.WriteLine("OK,请继续输入大写字母");
+                            continue;
+                        }
+                    }
+
+                    if (count == 5)
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.ReadLine();
+         }
+    }
+}
---
+++ test1/循环练习/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 循环练习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("for循环实现1到5的平方数输出");
+            for(int i = 1; i < 6; i++)
+            {
+                Console.Write(i * i + "  ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("do while循环实现1到5的平方数输出");
+            int j = 1;
+            do
+            {
+                Console.Write(j * j + "  ");
+                j++;
+            } while (j < 6);
+            Console.WriteLine();
+            Console.WriteLine("while循环实现1到5的平方数输出");
+            int k = 1;
+            while (k < 6)
+            {
+                Console.Write(k*k+"  ");
+                k++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
