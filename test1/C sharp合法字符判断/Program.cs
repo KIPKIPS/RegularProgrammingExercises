edc7e9ab0dc89551037f52a385e03621668ac8e0
KipKips <2432719656@qq.com>
2019-06-20 17:25:19 +0800

    C#合法字符判断

---
+++ test1/C sharp合法字符判断/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp合法字符判断
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("请输入变量名：");
+                string name = Console.ReadLine();
+                if ((name[0] >= 'a' && name[0] <= 'z') || (name[0] >= 'A' && name[0] <= 'Z') || (name[0] == '_') || (name[0] == '@'))
+                {
+                    for (int i = 1; i < name.Length; i++)
+                    {
+                        if (name[i] != '@')
+                        {
+                            if (i == name.Length-1)
+                            {
+                                Console.WriteLine("变量名合法");
+                                Console.WriteLine();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("变量名命名不合法");
+                            Console.WriteLine();
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("变量名命名不合法");
+                    Console.WriteLine();
+                }
+            }
+            //Console.ReadLine();
+        }
+    }
+}
