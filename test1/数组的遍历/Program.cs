3d52bd9e0abd124f41293331c85e3fe67816ca41
KipKips <2432719656@qq.com>
2019-06-20 09:54:10 +0800

    数组的循环遍历

---
+++ test1/数组的遍历/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 数组的遍历
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1,2,5,4,5,456,6,465,4,6546};
+            //for循环遍历
+            Console.WriteLine("for循环遍历");
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]+"  ");
+            }
+            Console.WriteLine();
+
+            //do while遍历
+            int j = 0;
+            Console.WriteLine("do while循环遍历");
+            do
+            {
+                Console.Write(array[j] + "  ");
+                j++;
+            }
+            while (j < array.Length);
+            Console.WriteLine();
+            
+            //while遍历
+            Console.WriteLine("while循环遍历");
+            int k = 0;
+            while (k < array.Length)
+            {
+                Console.Write(array[k] + "  ");
+                k++;
+            }
+            Console.WriteLine();
+
+            //foreach循环遍历
+            Console.WriteLine("foreach循环遍历");
+            foreach (int m in array)
+            {
+                Console.Write(m + "  ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- test1/结构体/Program.cs
+++ test1/结构体/Program.cs
@@ -8,8 +8,37 @@ namespace 结构体
 {
     class Program
     {
+        //声明向量的结构体类型
+        struct Vector3
+        {
+            public float x;
+            public float y;
+            public float z;
+        }
+        //声明方向的枚举类型
+        enum Direction
+        {
+            West,
+            East,
+            South,
+            North
+        }
+        //声明路径的结构体类型
+        struct Path
+        {
+            public float distance;
+            public Direction dir;
+        }
         static void Main(string[] args)
         {
+            //结构体使程序变得清晰，可读性提高
+            Vector3 position;
+            position.x = 64.5f;
+
+            //结构体和枚举类型的混合使用
+            Path path1;
+            path1.dir = Direction.East;
+            path1.distance = 1000;
         }
     }
 }
