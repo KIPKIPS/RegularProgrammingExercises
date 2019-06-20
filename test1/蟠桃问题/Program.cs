c739b31a71bb1489c5eecfcb8ea43a071f6ad802
KipKips <2432719656@qq.com>
2019-06-20 15:49:03 +0800

    蟠桃问题解决

---
+++ test1/蟠桃问题/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 蟠桃问题
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum=1;
+            string input = Console.ReadLine();
+            int num = Convert.ToInt32(input);
+            for(int i = 0; i < num-1; i++)
+            {
+                sum = (sum + 1) * 2;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
