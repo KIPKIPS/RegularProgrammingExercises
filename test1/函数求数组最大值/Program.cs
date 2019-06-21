f29fa2460ca8d275b14685d169f02e3f38572c3e
KipKips <2432719656@qq.com>
2019-06-21 15:01:36 +0800

    函数求数组最大值

---
+++ test1/函数求数组最大值/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 函数求数组最大值
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一串数字(以空格间隔):");
+            string input = Console.ReadLine();
+            string[] strTemp = input.Split(' ');
+            int[] num = new int[strTemp.Length];
+            for(int i = 0; i < strTemp.Length; i++)
+            {
+                num[i] = Convert.ToInt32(strTemp[i]);
+            }
+            int max = Max(num);
+            Console.WriteLine("数组中的最大值为: "+max);
+            Console.ReadLine();
+        }
+        static int Max(int[] array)
+        {
+            int max=array[0];
+            for(int i = 1; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
+    }
+}
