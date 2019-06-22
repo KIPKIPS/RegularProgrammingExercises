ecbf1fba30a3c4c529fc7c19afe3bb55bc0a02c0
KipKips <2432719656@qq.com>
2019-06-22 19:05:47 +0800

    构造函数使用

---
+++ CsharpIntermediate/构造函数/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 构造函数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentInfo me=new StudentInfo("王琨鹏",21);
+            Console.WriteLine("我的名字是"+me.ShowName()+",我今年"+me.ShowAge()+"岁了");
+            Console.WriteLine();
+
+            //通过属性访问类的字段值
+            me.Name = "aisohd";
+            me.Age = 85;
+            Console.WriteLine("我的名字是" + me.Name + ",我今年" + me.Age + "岁了");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CsharpIntermediate/构造函数/StudentInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 构造函数
+{
+    class StudentInfo
+    {
+        private string name;
+        private int age;
+
+        //有参的构造函数
+        public StudentInfo(string name,int age)
+        {
+            this.name = name;
+            this.age = age;
+        }  
+
+        public string ShowName()
+        {
+            return name;
+        }
+        public int ShowAge()
+        {
+            return age;
+        }
+
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }public int Age
+        {
+            set { age = value; }
+            get { return age; }
+        }
+    }
+}
