fc9b64370d62208fd8fc8713b45ee8b26bee806e
KipKips <2432719656@qq.com>
2019-06-24 21:33:27 +0800

    StringBuilder的使用

---
+++ CsharpAdvanced/StringBuilder使用/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilder使用
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder s=new StringBuilder("123",10);
+            s.Append("456");//Append方法将一串字符添加到之前的字符串的后面
+            Console.WriteLine(s);
+            Console.Read();
+        }
+    }
+}
