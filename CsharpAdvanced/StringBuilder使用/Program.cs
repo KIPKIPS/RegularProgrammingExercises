287d5954eb5fb4fb7ff48646fbde2f972b02295a
KipKips <2432719656@qq.com>
2019-06-25 11:34:35 +0800

    update

--- CsharpAdvanced/StringBuilder使用/Program.cs
+++ CsharpAdvanced/StringBuilder使用/Program.cs
@@ -12,6 +12,9 @@ namespace StringBuilder使用
         {
             StringBuilder s=new StringBuilder("123",10);
             s.Append("456");//Append方法将一串字符添加到之前的字符串的后面
+            s.Replace("123", "456");
+            s.Replace('5', ' ');
+
             Console.WriteLine(s);
             Console.Read();
         }
