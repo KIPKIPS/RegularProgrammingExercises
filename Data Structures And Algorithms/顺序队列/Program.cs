c6b1cc87994f3a29f4ac07039a9d9482077db187
RECNEPS-KIPS <2432719656@qq.com>
2019-07-15 14:22:48 +0800

    Update Program.cs

--- Data Structures And Algorithms/顺序队列/Program.cs
+++ Data Structures And Algorithms/顺序队列/Program.cs
@@ -8,7 +8,15 @@ namespace 顺序队列 {
     class Program {
         static void Main(string[] args) {
             Queue<int> queue=new Queue<int>();
+            for (int i = 4; i <= 32; i++) {
+                Console.WriteLine("case "+i+":audio.src = '../music/"+i+".mp3'; //音乐的路径");
+                Console.WriteLine("break;");
 
+            }
+            Console.ReadLine();
         }
     }
 }
+
+
+
