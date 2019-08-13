1a6da9c6b21bcaf1f51a5c608f076dd7a056d8c7
RECNEPS-KIPS <2432719656@qq.com>
2019-08-13 20:12:37 +0800

    次方问题

---
+++ 习题/求次方/求次方/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 求次方 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(Power(2,-1));
+            Console.ReadLine();
+        }
+        public static double Power(double thebase, int exponent) {
+            // write code here
+
+            return (double)Math.Pow(thebase, (double)exponent);
+
+
+        }
+
+    }
+}
