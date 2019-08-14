da02b0c168322004d384212886e256c5f4e06c9d
RECNEPS-KIPS <2432719656@qq.com>
2019-08-14 17:11:31 +0800

    模式串匹配问题

---
+++ 习题/模式串匹配问题/模式串匹配问题/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 模式串匹配问题 {
+    class Program {
+        static void Main(string[] args) {
+            string main = "ccccccinin";
+            string target = "in";
+            foreach (var VARIABLE in PatternStringMatching(main,1,target)) {
+                Console.Write("字符串 "+main+" 的第 "+VARIABLE+" 处存在模式串 "+target);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+
+        static List<int> PatternStringMatching(string main, int pos, string target) {
+            List<int>result=new List<int>();
+            for (int i = pos-1; i < main.Length; i++) {
+                if (main[i]==target[0]&&i+target.Length<=main.Length) {
+                    for (int j = i; j < target.Length+i; j++) {
+                        if (main[j]==target[j-i]) {
+                            if ((j-i+1)==target.Length) {
+                                result.Add(i + 1);
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
