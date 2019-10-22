d721e5fe8d345e6823e801ca0e3f4f29e856e045
RECNEPS-KIPS <2432719656@qq.com>
2019-10-22 22:28:10 +0800

    习题2道

---
+++ 2019校招真题在线编程/NO.107_派分糖果/NO.107_派分糖果/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NO._107_派分糖果 {
+    class Program {
+        static void Main(string[] args) {
+            string[] inp = Console.ReadLine().Split(',');
+            int[]num=new int[inp.Length];
+            for (int i = 0; i < inp.Length; i++) {
+                num[i] = int.Parse(inp[i]);
+            }
+            int[]res=new int[inp.Length];
+            res[0] = 1;
+            for (int i = 1; i < num.Length; i++) {
+                res[i] = 1;
+                if (num[i] > num[i - 1])
+                    res[i] = res[i - 1] + 1;
+            }
+            for (int i = num.Length - 2; i >= 0; i--) {
+                if (num[i] > num[i + 1])
+                    res[i] = Math.Max(res[i], res[i + 1] + 1);
+            }
+            Console.WriteLine(res.Sum());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2019校招真题在线编程/NO.108_种树/NO.108_种树/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NO._108_种树 {
+    class Program {
+        static void Main(string[] args) {
+            string nu = Console.ReadLine();
+            string[] input = Console.ReadLine().Split(' ');
+            List<int>num=new List<int>();
+            for (int i = 0; i < input.Length; i++) {
+                 num.Add(int.Parse(input[i]));
+            }
+            //操作计数器
+            int count = input.Length;
+            //数组下标计数器
+            int index = 0;
+            int[] treeNum=new int[input.Length];//记录树的个数
+            int[]treeIndex=new int[input.Length];//记录树的品种
+            while (count>0) {
+                treeNum[index] = num.Max();
+
+            }
+        }
+    }
+}
