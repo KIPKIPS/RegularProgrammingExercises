00677f7af72c58280ba250531789b6972f0250f2
RECNEPS-KIPS <2432719656@qq.com>
2019-11-20 00:35:48 +0800

    函数回调机制(委托)

--- Data Structures And Algorithms/回调函数机制_委托/回调函数机制_委托/Program.cs
+++ Data Structures And Algorithms/回调函数机制_委托/回调函数机制_委托/Program.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace 回调函数机制_委托 {
     class Program {
+        internal delegate void MyDelegate(int num);
+        private static MyDelegate myDelegate;
+
         static void Main(string[] args) {
+            //委托类型MyDelegate的实例myDelegate引用的方法是PrintNum
+            myDelegate = PrintNum;
+            myDelegate(50);
+        }
+
+        static void PrintNum(int num) {
+            Console.Write("PrintNum:" + num);
+        }
+        static void DoubleNum(int num) {
+            Console.Write("DoubleNum:" + num * 2);
         }
     }
 }
