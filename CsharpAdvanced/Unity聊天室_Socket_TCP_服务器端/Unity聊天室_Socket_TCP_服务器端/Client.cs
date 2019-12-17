31c39d63989eb4e22a1de358581ca6fcdbcfec99
RECNEPS-KIPS <2432719656@qq.com>
2019-12-17 15:36:30 +0800

    聊天室

--- CsharpAdvanced/Unity聊天室_Socket_TCP_服务器端/Unity聊天室_Socket_TCP_服务器端/Client.cs
+++ CsharpAdvanced/Unity聊天室_Socket_TCP_服务器端/Unity聊天室_Socket_TCP_服务器端/Client.cs
@@ -9,7 +9,7 @@ namespace Unity聊天室_Socket_TCP_服务器端 {
     public class Client {
         private Socket clientSocket;
         private Thread t;
-        private Byte[] data=new byte[1024];//数据容器
+        private byte[] data=new byte[1024];//数据容器
         public Client(Socket s) {
             clientSocket = s;
             t =new Thread(ReceiveMessage);
@@ -19,15 +19,23 @@ namespace Unity聊天室_Socket_TCP_服务器端 {
         private void ReceiveMessage() {
             //持续接收客户端的数据
             while (true) {
-                //接收数据时判断连接是否断开
+                //接收数据时判断连接是否断开,断开返回true,连接状态返回false
                 if (clientSocket.Poll(10, SelectMode.SelectRead)) {
+                    Console.WriteLine("Connected is shutdown");
+                    clientSocket.Close();//关闭连接
                     break;//跳出循环,终止线程的执行
                 }
                 int length = clientSocket.Receive(data);
                 string message = Encoding.UTF8.GetString(data, 0, length);
                 //TODO:接收到数据的时候,把这个数据分发到客户端;
-                Console.WriteLine("Receive a message : " + message);
+                if (message!="") {
+                    Console.WriteLine("Receive a message : " + message);
+                }
             }
+        }
+
+        public void Send(string message) {
+            clientSocket.Send(Encoding.UTF8.GetBytes(message));
         }
     }
 }
