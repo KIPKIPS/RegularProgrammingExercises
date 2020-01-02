18e762af31c01a3974da83948b6c8fe57ea9e65e
RECNEPS-KIPS <2432719656@qq.com>
2020-01-02 21:50:52 +0800

    事件和回调函数

---
+++ CsharpAdvanced/事件/事件/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 事件 {
+    /// <summary>
+    /// 委托
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    public delegate void ProcessDelegate(object sender, EventArgs e);
+    class Program {
+        static void Main(string[] args) {
+            /*  第一步执行  */
+            Test t = new Test();
+            /* 关联事件方法，相当于寻找到了委托人 */
+            t.ProcessEvent += new ProcessDelegate(t_ProcessEvent);
+            /* 进入Process方法 */
+            Console.WriteLine(t.Process());
+
+            Console.Read();
+        }
+
+        static void t_ProcessEvent(object sender, EventArgs e) {
+            Test t = (Test)sender;
+            t.Text1 = "Hello";
+            t.Text2 = "World";
+        }
+    }
+
+    public class Test {
+
+        public string Text1 { get; set; }
+        public string Text2 { get; set; }
+
+        public event ProcessDelegate ProcessEvent;
+
+        void ProcessAction(object sender, EventArgs e) {
+            if (ProcessEvent == null)
+                ProcessEvent += new ProcessDelegate(t_ProcessEvent);
+            ProcessEvent(sender, e);
+        }
+
+        //如果没有自己指定关联方法，将会调用该方法抛出错误
+        void t_ProcessEvent(object sender, EventArgs e) {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        void OnProcess() {
+            ProcessAction(this, EventArgs.Empty);
+        }
+
+        public string Process() {
+            OnProcess();
+            return Text1 + Text2;
+        }
+    }
+}
---
+++ CsharpAdvanced/回调函数/回调函数/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 回调函数 {
+    /// <summary>
+    /// 委托
+    /// </summary>
+    /// <param name="s1"></param>
+    /// <param name="s2"></param>
+    /// <returns></returns>
+    public delegate string ProcessDelegate(string s1, string s2);
+    class Program {
+        static void Main(string[] args) {
+            /*  调用方法  */
+            Worker worker=new Worker();
+            string r1 = worker.Function("Text1", "Text2", new ProcessDelegate(worker.Process1));
+            string r2 = worker.Function("Text1", "Text2", new ProcessDelegate(worker.Process2));
+            string r3 = worker.Function("Text1", "Text2", new ProcessDelegate(worker.Process3));
+
+            Console.WriteLine(r1);
+            Console.WriteLine(r2);
+            Console.WriteLine(r3);
+            Console.ReadLine();
+        }
+    }
+    public class Worker {
+        //该方法将任务交给Worker的注册方法去执行,至于需要哪个注册方法,则在调用时由用户去设定
+        public string Function(string s1, string s2, ProcessDelegate process) {
+            return process(s1, s2);
+        }
+        //Worker的注册方法列表
+        public string Process1(string s1, string s2) {
+            return s1 + s2+ " Process1";
+        }
+
+        public string Process2(string s1, string s2) {
+            return s1 + s2+ " Process2";
+        }
+
+        public string Process3(string s1, string s2) {
+            return s1 + s2+ " Process3";
+        }
+    }
+}
