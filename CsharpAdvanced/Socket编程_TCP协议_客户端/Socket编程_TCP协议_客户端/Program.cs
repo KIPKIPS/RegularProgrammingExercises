1118bdd8ca6b1e365519c24727a23da67ea74d22
RECNEPS-KIPS <2432719656@qq.com>
2020-01-03 19:23:16 +0800

    实现Socket通信流程

--- CsharpAdvanced/Socket编程_TCP协议_客户端/Socket编程_TCP协议_客户端/Program.cs
+++ CsharpAdvanced/Socket编程_TCP协议_客户端/Socket编程_TCP协议_客户端/Program.cs
@@ -12,13 +12,13 @@ namespace Socket编程_TCP协议_客户端 {
             //创建socket
             Socket tcpClient=new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
             //发起建立连接的请求
-            IPAddress ipAddress = IPAddress.Parse("10.17.129.238");
+            IPAddress ipAddress = IPAddress.Parse("10.18.59.166");
             EndPoint point=new IPEndPoint(ipAddress,7788);
             tcpClient.Connect(point);//通过IP和端口号连接到服务器
             Byte[]data=new byte[1024];
             int length=tcpClient.Receive(data);//存取数据
             //length返回值表示接受了多少的数据
-            Console.WriteLine("Receive A Message: ");
+            Console.WriteLine("Receive a message: ");
             string message=Encoding.UTF8.GetString(data,0,length);//从0开始,到length结束,只把接受到的数据转换
             Console.WriteLine(message);
 
@@ -28,7 +28,7 @@ namespace Socket编程_TCP协议_客户端 {
                 if (send=="end"||send=="") {
                     break;
                 }
-                Console.WriteLine("Send A Message:");
+                Console.WriteLine("Send a message:");
                 tcpClient.Send(Encoding.UTF8.GetBytes(send));//发送客户端用户的输入
             }
             
