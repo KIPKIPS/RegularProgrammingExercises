2004d340395a015dbead459de8b4cbec682b8a2b
RECNEPS-KIPS <2432719656@qq.com>
2020-01-10 20:28:38 +0800

    增添了转换矩阵
    1.模型到世界矩阵
    2.世界到摄像机矩阵.
    相机到投影矩阵

--- Triangle_Rotate/3DTransform/Form1.Designer.cs
+++ Triangle_Rotate/3DTransform/Form1.Designer.cs
@@ -25,6 +25,8 @@
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // timer1
@@ -33,22 +35,37 @@
             this.timer1.Interval = 40;
             this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(23, 25);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = 250;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(395, 56);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 250;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1__Scroll);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(782, 753);
+            this.Controls.Add(this.trackBar1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
     }
 }
 
--- Triangle_Rotate/3DTransform/Form1.cs
+++ Triangle_Rotate/3DTransform/Form1.cs
@@ -13,24 +13,39 @@ namespace _3DTransform {
         private Triangle3D t;
         private Matrix4x4 m_scale;
         private Matrix4x4 m_rotate;
+        public Matrix4x4 m_view;//摄像机矩阵
+        private Matrix4x4 m_projection;//投影矩阵
         private int degree;
         public Form1() {
             InitializeComponent();
-            m_scale=new Matrix4x4();
+            m_scale = new Matrix4x4();
             m_scale[1, 1] = 200;
             m_scale[2, 2] = 200;
-            m_scale[3,3] = 200;
-            m_scale[4,4] = 1;
+            m_scale[3, 3] = 200;
+            m_scale[4, 4] = 1;
 
-            m_rotate=new Matrix4x4();
-            ;
+            //摄像机矩阵
+            m_view = new Matrix4x4();
+            m_view[1, 1] = 1;
+            m_view[2, 2] = 1;
+            m_view[3, 3] = 1;
+            m_view[4, 3] = 250;
+            m_view[4, 4] = 1;
+
+            m_projection = new Matrix4x4();
+            m_projection[1, 1] = 1;
+            m_projection[2, 2] = 1;
+            m_projection[3, 3] = 1;
+            m_projection[3, 4] = 1 / 250.0;
+
+            m_rotate = new Matrix4x4();
         }
 
         private void Form1_Load(object sender, EventArgs e) {
-            Vector4 a=new Vector4(0,-0.5,0,1);
+            Vector4 a = new Vector4(0, -0.5, 0, 1);
             Vector4 b = new Vector4(0.5, 0.5, 0, 1);
             Vector4 c = new Vector4(-0.5, 0.5, 0, 1);
-            t =new Triangle3D(a,b,c);
+            t = new Triangle3D(a, b, c);
 
         }
 
@@ -40,34 +55,42 @@ namespace _3DTransform {
 
         private void Timer1_Tick(object sender, EventArgs e) {
             degree += 2;
+            degree = degree %720;
             double angle = degree / 360.0 * Math.PI;
             //绕Y轴旋转矩阵
-            //m_rotate[1, 1] = Math.Cos(angle);
-            //m_rotate[1, 3] = Math.Sin(angle);
-            //m_rotate[2, 2] = 1;
-            //m_rotate[3, 1] = -Math.Sin(angle);
-            //m_rotate[3, 3] = Math.Cos(angle);
-            //m_rotate[4, 4] = 1;
+            m_rotate[1, 1] = Math.Cos(angle);
+            m_rotate[1, 3] = Math.Sin(angle);
+            m_rotate[2, 2] = 1;
+            m_rotate[3, 1] = -Math.Sin(angle);
+            m_rotate[3, 3] = Math.Cos(angle);
+            m_rotate[4, 4] = 1;
 
             //绕X轴旋转矩阵
-            //m_rotate[1, 1] = 1;
-            //m_rotate[2,2] = Math.Cos(angle);
-            //m_rotate[2,3] = Math.Sin(angle);
-            //m_rotate[3,2] = -Math.Sin(angle);
-            //m_rotate[3,3] = Math.Cos(angle);
-            //m_rotate[4,4] = 1;
+            m_rotate[1, 1] = 1;
+            m_rotate[2,2] = Math.Cos(angle);
+            m_rotate[2,3] = Math.Sin(angle);
+            m_rotate[3,2] = -Math.Sin(angle);
+            m_rotate[3,3] = Math.Cos(angle);
+            m_rotate[4,4] = 1;
 
             //绕Z轴旋转矩阵
             m_rotate[1, 1] = Math.Cos(angle);
-            m_rotate[1,2] = Math.Sin(angle);
-            m_rotate[2,1] = -Math.Sin(angle);
+             m_rotate[1,2] = Math.Sin(angle);
+             m_rotate[2,1] = -Math.Sin(angle);
             m_rotate[2,2] = Math.Cos(angle);
             m_rotate[3,3] = 1;
             m_rotate[4,4] = 1;
 
             Matrix4x4 m = m_scale.Mul(m_rotate);
+            m = m.Mul(m_view);
+            m = m.Mul(m_projection);
             t.Transform(m);//传入缩放+旋转矩阵
             this.Invalidate();
+        }
+
+
+        private void TrackBar1__Scroll(object sender, EventArgs e) {
+            m_view[4, 3] = (sender as TrackBar).Value;
         }
     }
 }
