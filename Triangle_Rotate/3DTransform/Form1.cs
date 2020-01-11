b0833628da74ea50da0950d68f7b38392ac3e16b
RECNEPS-KIPS <2432719656@qq.com>
2020-01-11 19:06:28 +0800

    将XYZ方向的旋转矩阵分离成三个分量矩阵

--- Triangle_Rotate/3DTransform/Form1.cs
+++ Triangle_Rotate/3DTransform/Form1.cs
@@ -12,7 +12,11 @@ namespace _3DTransform {
     public partial class Form1 : Form {
         private Triangle3D t;
         private Matrix4x4 m_scale;
-        private Matrix4x4 m_rotate;
+
+        private Matrix4x4 m_rotateX;
+        private Matrix4x4 m_rotateY;
+        private Matrix4x4 m_rotateZ;
+
         public Matrix4x4 m_view;//摄像机矩阵
         private Matrix4x4 m_projection;//投影矩阵
         private int degree;
@@ -38,7 +42,9 @@ namespace _3DTransform {
             m_projection[3, 3] = 1;
             m_projection[3, 4] = 1 / 250.0;
 
-            m_rotate = new Matrix4x4();
+            m_rotateX = new Matrix4x4();
+            m_rotateY = new Matrix4x4();
+            m_rotateZ = new Matrix4x4();
         }
 
         private void Form1_Load(object sender, EventArgs e) {
@@ -55,42 +61,50 @@ namespace _3DTransform {
 
         private void Timer1_Tick(object sender, EventArgs e) {
             degree += 2;
-            degree = degree %720;
+            degree = degree % 720;
             double angle = degree / 360.0 * Math.PI;
-            //绕Y轴旋转矩阵
-            m_rotate[1, 1] = Math.Cos(angle);
-            m_rotate[1, 3] = Math.Sin(angle);
-            m_rotate[2, 2] = 1;
-            m_rotate[3, 1] = -Math.Sin(angle);
-            m_rotate[3, 3] = Math.Cos(angle);
-            m_rotate[4, 4] = 1;
-
             //绕X轴旋转矩阵
-            m_rotate[1, 1] = 1;
-            m_rotate[2,2] = Math.Cos(angle);
-            m_rotate[2,3] = Math.Sin(angle);
-            m_rotate[3,2] = -Math.Sin(angle);
-            m_rotate[3,3] = Math.Cos(angle);
-            m_rotate[4,4] = 1;
+            m_rotateX[1, 1] = 1;
+            m_rotateX[2, 2] = Math.Cos(angle);
+            m_rotateX[2, 3] = Math.Sin(angle);
+            m_rotateX[3, 2] = -Math.Sin(angle);
+            m_rotateX[3, 3] = Math.Cos(angle);
+            m_rotateX[4, 4] = 1;
+
+            //绕Y轴旋转矩阵
+            m_rotateY[1, 1] = Math.Cos(angle);
+            m_rotateY[1, 3] = Math.Sin(angle);
+            m_rotateY[2, 2] = 1;
+            m_rotateY[3, 1] = -Math.Sin(angle);
+            m_rotateY[3, 3] = Math.Cos(angle);
+            m_rotateY[4, 4] = 1;
 
             //绕Z轴旋转矩阵
-            m_rotate[1, 1] = Math.Cos(angle);
-             m_rotate[1,2] = Math.Sin(angle);
-             m_rotate[2,1] = -Math.Sin(angle);
-            m_rotate[2,2] = Math.Cos(angle);
-            m_rotate[3,3] = 1;
-            m_rotate[4,4] = 1;
+            m_rotateZ[1, 1] = Math.Cos(angle);
+            m_rotateZ[1, 2] = Math.Sin(angle);
+            m_rotateZ[2, 1] = -Math.Sin(angle);
+            m_rotateZ[2, 2] = Math.Cos(angle);
+            m_rotateZ[3, 3] = 1;
+            m_rotateZ[4, 4] = 1;
 
-            Matrix4x4 m = m_scale.Mul(m_rotate);//模型到世界矩阵
-            m = m.Mul(m_view);//世界到摄像机矩阵
-            m = m.Mul(m_projection);//相机到投影矩阵
+            //模型到世界矩阵
+            Matrix4x4 m = m_scale.Mul(m_rotateX);
+            m = m.Mul(m_rotateY);
+            m = m.Mul(m_rotateZ);
+
+            //世界到摄像机矩阵
+            m = m.Mul(m_view);
+            //相机到投影矩阵
+            m = m.Mul(m_projection);
             t.Transform(m);//传入缩放+旋转矩阵
             this.Invalidate();
         }
 
 
         private void TrackBar1__Scroll(object sender, EventArgs e) {
-            m_view[4, 3] = (sender as TrackBar).Value;
+            m_view[4, 3] = (sender as TrackBar).Value;//根据滑条的值改变相机距离图形的距离
         }
+        //TODO:撤销矩阵的变换
+
     }
 }
