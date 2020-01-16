82df0a3ad8c58b0dc9e0647df26983ee4dbc4e7f
RECNEPS-KIPS <2432719656@qq.com>
2020-01-16 17:21:05 +0800

    实现向量的点乘,叉乘,减法操作

--- Triangle_Rotate/3DTransform/Vector4.cs
+++ Triangle_Rotate/3DTransform/Vector4.cs
@@ -19,5 +19,17 @@
             z = v.z;
             w = v.w;
         }
+        //四维向量的减法操作符运算
+        public static Vector4 operator -(Vector4 a, Vector4 b) {
+            return new Vector4(a.x-b.x,a.y-b.y,a.z-b.z,a.w-b.w);
+        }
+        //向量叉乘
+        public Vector4 Cross(Vector4 v) {
+            return new Vector4(this.y*v.z-this.z*v.y,v.x*this.z-this.x*v.z,this.x*v.y-v.x*this.y,0);
+        }
+        //向量点乘
+        public float Dot(Vector4 v) {
+            return (float) (this.x * v.x + this.y * v.y + this.z * v.z);
+        }
     }
 }
