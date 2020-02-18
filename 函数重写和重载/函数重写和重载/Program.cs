c2c022abc10b8abf97e34807879bd8b943acab4c
RECNEPS-KIPS <2432719656@qq.com>
2020-02-18 12:10:56 +0800

    函数重载的用法

---
+++ 函数重写和重载/函数重写和重载/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 函数重写和重载 {
+    class Program {
+        static void Main(string[] args) {
+        }
+
+        //函数参数不同
+        int Function(int a,int b) {
+            return a + b;
+        }
+        int Function(int a) {
+            return a;
+        }
+        //函数参数不同,返回值不同
+        double FunctionA(int a, int b) {
+            return a + b;
+        }
+        int FunctionA(int a) {
+            return a;
+        }
+    }
+}
