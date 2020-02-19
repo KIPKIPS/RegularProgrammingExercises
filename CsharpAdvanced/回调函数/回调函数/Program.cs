639b5f1cd5514e4655c41bb84b4357b51c4d4f8d
RECNEPS-KIPS <2432719656@qq.com>
2020-02-19 17:25:31 +0800

    回调函数

--- CsharpAdvanced/回调函数/回调函数/Program.cs
+++ CsharpAdvanced/回调函数/回调函数/Program.cs
@@ -5,20 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace 回调函数 {
-    /// <summary>
-    /// 委托
-    /// </summary>
-    /// <param name="s1"></param>
-    /// <param name="s2"></param>
-    /// <returns></returns>
     public delegate string ProcessDelegate(string s1, string s2);
     class Program {
         static void Main(string[] args) {
-            /*  调用方法  */
-            Worker worker=new Worker();
-            string r1 = worker.Function("Text1", "Text2", new ProcessDelegate(worker.Process1));
-            string r2 = worker.Function("Text1", "Text2", new ProcessDelegate(worker.Process2));
-            string r3 = worker.Function("Text1", "Text2", new ProcessDelegate(worker.Process3));
+            //调用方法
+            string r1 = Worker.Function("Text1", "Text2", Worker.Process1);
+            string r2 = Worker.Function("Text1", "Text2", Worker.Process2);
+            string r3 = Worker.Function("Text1", "Text2", Worker.Process3);
 
             Console.WriteLine(r1);
             Console.WriteLine(r2);
@@ -28,19 +21,17 @@ namespace 回调函数 {
     }
     public class Worker {
         //该方法将任务交给Worker的注册方法去执行,至于需要哪个注册方法,则在调用时由用户去设定
-        public string Function(string s1, string s2, ProcessDelegate process) {
+        public static string Function(string s1, string s2, ProcessDelegate process) {
             return process(s1, s2);
         }
         //Worker的注册方法列表
-        public string Process1(string s1, string s2) {
+        public static string Process1(string s1, string s2) {
             return s1 + s2+ " Process1";
         }
-
-        public string Process2(string s1, string s2) {
+        public static string Process2(string s1, string s2) {
             return s1 + s2+ " Process2";
         }
-
-        public string Process3(string s1, string s2) {
+        public static string Process3(string s1, string s2) {
             return s1 + s2+ " Process3";
         }
     }
