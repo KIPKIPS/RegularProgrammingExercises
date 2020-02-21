d34b9309e4595b6f7f65d9b8f4eb1f54c7d62c02
RECNEPS-KIPS <2432719656@qq.com>
2020-02-21 15:19:53 +0800

    最大子串问题

---
+++ 最大子串问题/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            string tar = Console.ReadLine();
+            char[] tarCharArray = tar.ToCharArray();
+            Array.Sort(tarCharArray);
+            Array.Reverse(tarCharArray);
+            string d = "";
+            string result = "";
+            HashSet<char> dic=new HashSet<char>();
+            foreach (char VARIABLE in tarCharArray) {
+                if (!dic.Contains(VARIABLE)) {
+                    dic.Add(VARIABLE);
+                    d += VARIABLE;
+                }
+            }
+            //Console.WriteLine("降序字典"+d);
+            for (int i = 0; i < tar.Length; i++) {
+                for (int j = 0; j < d.Length; j++) {
+                    if (j > 0) {
+                        if (tar[i] == d[j] && i > IndexOfLastChar(d[j - 1], tar)) {
+                            result += tar[i];
+                        }
+                    }
+                    else {
+                        if (tar[i] == d[j]) {
+                            result += tar[i];
+                        }
+                    }
+                    
+                }
+            }
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        //最后一个字符在串中的位置
+        static int IndexOfLastChar(char tar, string s) {
+            for (int i = s.Length-1; i >=0; i--) {
+                if (s[i]==tar) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
