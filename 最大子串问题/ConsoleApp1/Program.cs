db1ae90b4a1ea84137013777813381970feb2a97
RECNEPS-KIPS <2432719656@qq.com>
2020-02-23 20:19:54 +0800

    结构优化

--- 最大子串问题/ConsoleApp1/Program.cs
+++ 最大子串问题/ConsoleApp1/Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 
 namespace ConsoleApp1 {
     class Program {
-        static void Main(string[] args) {
+        static void Main() {
             string tar = Console.ReadLine();//读取控制台输入
             char[] tarCharArray = tar.ToCharArray();//将字符串转化成字符数组
             Array.Sort(tarCharArray);//字符数组排序
             Array.Reverse(tarCharArray);//逆置
             string sortStr = "";
             string result = "";//存储最大子串
-            HashSet<char> dic=new HashSet<char>();//字符串的hash表
+            HashSet<char> dic = new HashSet<char>();//字符串的hash表
             foreach (char VARIABLE in tarCharArray) {
                 if (!dic.Contains(VARIABLE)) {
                     dic.Add(VARIABLE);
@@ -21,16 +21,13 @@ namespace ConsoleApp1 {
             for (int i = 0; i < tar.Length; i++) {
                 for (int j = 0; j < sortStr.Length; j++) {
                     if (j > 0) {
-                        if (tar[i] == sortStr[j] && i > IndexOfLastChar(sortStr[j - 1], tar)) {
+                        if (tar[i] == sortStr[j] && i > IndexOfLastChar(sortStr[j - 1], tar))
                             result += tar[i];
-                        }
                     }
                     else {
-                        if (tar[i] == sortStr[j]) {
+                        if (tar[i] == sortStr[j])
                             result += tar[i];
-                        }
                     }
-                    
                 }
             }
             Console.WriteLine(result);
@@ -38,11 +35,9 @@ namespace ConsoleApp1 {
         }
         //最后一个字符在串中的位置
         static int IndexOfLastChar(char tar, string s) {
-            for (int i = s.Length-1; i >=0; i--) {
-                if (s[i]==tar) {
+            for (int i = s.Length - 1; i >= 0; i--)
+                if (s[i] == tar)
                     return i;
-                }
-            }
             return -1;
         }
     }
