90ef756c6e68a63683e16cfac086fc82b54bfdab
RECNEPS-KIPS <2432719656@qq.com>
2020-03-07 13:12:47 +0800

    习题两道

---
+++ 华为笔试题/华为笔试题/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 华为笔试题 {
+    class Program {
+        static void Main(string[] args) {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int[] data=new int[num];
+            
+            for (int i = 0; i < data.Length; i++) {
+                data[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            Console.WriteLine(Fun(data));
+            Console.ReadLine();
+        }
+
+        static int Fun(int[]data) {
+            if (data.Length<=1) {
+                return data.Length;
+            }
+            int res = 1;
+            int front = 0;
+            int step = data[0];
+            for (int i = 0; i < data.Length; i++) {
+                int max = data[front + 1];
+                int tempStep = 0;
+                int tempFront = 0;
+                for (int j = front + 1; j <= front + step; j++) {
+                    if (front + step>data.Length-1) {
+                        return res + 1;
+                    }
+                    else {
+                        if (data[j] >= max) {
+                            max = data[j];
+                            tempFront = j;
+                            tempStep = max;
+                        }
+                    }
+                    
+                }
+                front = tempFront;
+                step = tempStep;
+                if (front + step >= data.Length - 1) {
+                    return res+1;
+                }
+                else {
+                    res++;
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ 华为笔试题/华为笔试题二/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 华为笔试题二 {
+    class Program {
+        static void Main(string[] args) {
+            List<int[]> data=new List<int[]>();
+            while (true) {
+                string temp= Console.ReadLine();
+                if (temp=="") {
+                    break;
+                }
+                else {
+                    string[] tempArr = temp.Split(' ');
+                    int[] tempIntArr=new int[tempArr.Length];
+                    for (int i = 0; i < tempArr.Length; i++) {
+                        tempIntArr[i] = Convert.ToInt32(tempArr[i])>=10?1:0;
+                    }
+                    data.Add(tempIntArr);
+                }
+            }
+            List<string>res=new List<string>();
+            for (int i = 0; i < data.Count; i++) {
+                bool getJG = false;
+                bool get = false;
+                //01 间隔出现
+                for (int j = 0; j < data[i].Length-1; j++) {
+                    if (Math.Abs(data[i][j + 1] - data[i][j]) != 1) {
+                        break;
+                    }
+                    else {
+                        if (j==data[i].Length-2) {
+                            res.Add("true");
+                            getJG = true;
+                            break;
+                        }
+                    }
+                }
+                if (getJG) {
+                    continue;
+                }
+                
+                //首尾0中1
+                if (data[i][0]==0&&data[i][data[i].Length-1]==0) {
+                    for (int j = 1; j < data[i].Length-1; j++) {
+                        if (data[i][j]!=1) {
+                            res.Add("false");
+                            break;
+                        }
+                        else {
+                            if (j== data[i].Length - 2) {
+                                res.Add("true");
+                                get = true;
+                            }
+                        }
+                    }
+                }
+                if (get) {
+                    continue;
+                }
+                //首尾1中0
+                if (data[i][0] == 1 && data[i][data[i].Length - 1] == 1) {
+                    for (int j = 1; j < data[i].Length - 1; j++) {
+                        if (data[i][j] != 0) {
+                            res.Add("false");
+                            break;
+                        }
+                        else {
+                            if (j == data[i].Length - 2) {
+                                res.Add("true");
+                            }
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < res.Count; i++) {
+                if (i==res.Count-1) {
+                    Console.Write(res[i]);
+                }
+                else {
+                    Console.Write(res[i] + " ");
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
