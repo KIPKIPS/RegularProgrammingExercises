e88c8768a8476129f956ff076c1d530c01e6017f
RECNEPS-KIPS <2432719656@qq.com>
2020-03-15 17:28:48 +0800

    习题订正

--- 字节跳动练习题2019/Problem_02/Program.cs
+++ 字节跳动练习题2019/Problem_02/Program.cs
@@ -10,22 +10,22 @@ namespace Problem_02 {
             string[] inp = Console.ReadLine().Split(' ');
             int buildNum = Convert.ToInt32(inp[0]);
             int maxDis = Convert.ToInt32(inp[1]);
-            int[] buildData = new int [buildNum];
+            int[] buildData = new int[buildNum];
             string[] t = Console.ReadLine().Split(' ');
-            for (int k = 0; k < t.Length; k++) {
-                buildData[k] = Convert.ToInt32(t[k]);
+            for (int i = 0; i < t.Length; i++) {
+                buildData[i] = Convert.ToInt32(t[i]);
             }
 
-            int res = 0;
-            int i = 0;
-            while (i < buildData.Length - 2) {
-                int j = i + 2;
-                while (j< buildData.Length&& buildData[j] - buildData[i] <= maxDis) {
+            long res = 0;
+            int j;
+            for (int i = 0; i < buildData.Length-2; i++) {
+                 j = i + 2;
+                while (j < buildData.Length&& buildData[j] - buildData[i] <= maxDis) {
                     res += j - i - 1;
                     j++;
                 }
-                i++;
             }
+
             Console.WriteLine(res);
             Console.ReadLine();
         }
