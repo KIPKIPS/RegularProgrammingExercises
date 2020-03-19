2d5048cf6979587297d6429a18815fc413c81e4b
RECNEPS-KIPS <2432719656@qq.com>
2020-03-19 15:49:06 +0800

    实现二分法查找

---
+++ 数据结构常见算法薄弱点攻坚战/二分法/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 二分法 {
+    class Program {
+        static void Main(string[] args) {
+            string[] inp = Console.ReadLine().Split(' ');
+            int[] data=new int[inp.Length];
+            for (int i = 0; i < data.Length; i++) {
+                data[i] = Convert.ToInt32(inp[i]);
+            }
+
+            Console.WriteLine(Search(data,5));
+            Console.ReadLine();
+        }
+        //二分查找
+        static int Search(int[] data,int key) {
+            int low = 0;
+            int mid = -1;
+            int high = data.Length - 1;
+            while (low<=high) {
+                mid = (low + high) / 2;
+                if (data[mid]==key) {
+                    return mid;
+                }
+                else if (data[mid]>key) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+            return mid;
+        }
+    }
+}
