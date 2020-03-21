c9d3feaf91192e3a324b1b9034633a9f108892a0
RECNEPS-KIPS <2432719656@qq.com>
2020-03-22 00:57:59 +0800

    循环链表判断

---
+++ Works for 2020/循环链表/循环链表/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 循环链表 {
+    class Program {
+        class Node {
+            public int data;
+            public Node next;
+            public Node(int x) {
+                data = x;
+            }
+        }
+
+        static void Main(string[] args) {
+            Node a=new Node(1);
+            Node b = new Node(2);
+            Node c = new Node(3);
+            Node d = new Node(4);
+            a.next = b;b.next = c;c.next = d;d.next = b;
+            Console.WriteLine(IsLoopList(a));
+            Console.ReadLine();
+        }
+
+        static bool IsLoopList(Node head) {
+            HashSet<Node>map=new HashSet<Node>();
+            Node temp = head;
+            while (temp!=null) {
+                if (!map.Contains(temp)) {
+                    map.Add(temp);
+                    temp = temp.next;
+                }
+                else {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
