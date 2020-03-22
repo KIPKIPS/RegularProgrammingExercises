65474ef9a1edb89893ee4cfd9e5f41b8edafc577
RECNEPS-KIPS <2432719656@qq.com>
2020-03-22 11:27:30 +0800

    大数相乘

---
+++ Works for 2020/疯狂游戏笔试题/Problem_01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_01 {
+    class Program {
+        static void Main(string[] args) {
+            string[] data = Console.ReadLine().Split('*');
+            long res = 1;
+            for (int i = 0; i < data.Length; i++) {
+                res*= long.Parse(data[i]);
+            }
+
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
