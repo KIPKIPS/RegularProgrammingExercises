280da34931fbcd4166722ed761845b79c492d721
RECNEPS-KIPS <2432719656@qq.com>
2020-03-23 20:46:47 +0800

    节点建立边的关系

---
+++ Works for 2020/3月23日练习题/图的实现/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 图的实现 {
+    class Program {
+        static void Main(string[] args) {
+            Graph g=new Graph(5);
+            g.AddVertex(1);
+
+        }
+    }
+}
