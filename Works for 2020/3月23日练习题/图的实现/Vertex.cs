bb1f61cfab64f35eeab78a8f451559a1ea1bf598
RECNEPS-KIPS <2432719656@qq.com>
2020-03-23 20:47:00 +0800

    显示节点

---
+++ Works for 2020/3月23日练习题/图的实现/Vertex.cs
@@ -0,0 +1,9 @@
+﻿namespace 图的实现 {
+    public class Vertex {
+        public int data;
+        public bool isVisited;
+        public Vertex(int data) {
+            this.data = data;
+        }
+    }
+}
