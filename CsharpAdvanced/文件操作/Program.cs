89871e6ef2b9425c13e9ff4a2d2d81a661e4e751
RECNEPS-KIPS <2432719656@qq.com>
2020-03-26 15:13:24 +0800

    文件操作,创建文件

---
+++ CsharpAdvanced/文件操作/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 文件操作 {
+    class Program {
+        static void Main(string[] args) {
+            FileInfo file=new FileInfo("File.txt");//相对路径
+            Console.WriteLine(file.Exists);//是否存在,判断编译后的路径,Debug文件夹下
+
+            //属性
+            FileInfo file_1 = new FileInfo(@"E:\Visual_Studio_WorkStation\CsharpAdvanced\文件操作\FileTest.txt");//绝对路径
+            Console.WriteLine(file_1.Exists);
+            Console.WriteLine(file_1.Directory);//获取路径
+            Console.WriteLine(file_1.Length);//取得文件长度,字节数
+            Console.WriteLine(file_1.IsReadOnly);//判断文件是否只读
+            Console.WriteLine(file_1.Attributes);//获取特性
+
+            //方法
+            //file_1.Delete();//删除文件
+            file_1.CopyTo("Copy.txt");//将file_1复制一份,存为Copy.txt
+            Console.ReadLine();
+        }
+    }
+}
