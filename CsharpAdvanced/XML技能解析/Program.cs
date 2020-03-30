58ce81cc3dd00f3c59870538f26f645f1d1b35fa
RECNEPS-KIPS <2432719656@qq.com>
2020-03-30 15:43:25 +0800

    XML习题

---
+++ CsharpAdvanced/XML技能解析/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XML技能解析 {
+    class Program {
+        static void Main(string[] args) {
+            List<Skill>skills=new List<Skill>();
+            XmlDocument xmlDoc=new XmlDocument();
+            xmlDoc.Load("xml技能信息.xml");
+            XmlNodeList skillList = xmlDoc.FirstChild.FirstChild.ChildNodes;//获取技能信息的列表
+            foreach (XmlNode skill in skillList) {
+                Skill temp=new Skill();
+                //获取子标签的值
+                if (skill["Name"]!=null) {
+                    string name =skill["Name"].InnerText;
+                    temp.Name = name;
+                }
+                //获取子标签的属性集合
+                XmlAttributeCollection xac = skill.Attributes;
+                if (xac!=null) {
+                    temp.ID = Convert.ToInt32(xac["SkillID"].Value);
+                    temp.EnglishName = xac["SkillEngName"].Value;
+                    temp.TriggerType = Convert.ToInt32(xac["TriggerType"].Value);
+                    temp.Image = xac["ImageFile"].Value;
+                    temp.AvailableRace = Convert.ToInt32(xac["AvailableRace"].Value);
+                }
+                skills.Add(temp);
+            }
+            foreach (Skill skill in skills) {
+                Console.WriteLine(skill.ID + "\t" + skill.Name + "\t" + skill.EnglishName + "\t" + skill.TriggerType + "\t" + skill.Image + "\t" + skill.AvailableRace);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CsharpAdvanced/XML技能解析/Skill.cs
@@ -0,0 +1,10 @@
+﻿namespace XML技能解析 {
+    public class Skill {
+        public int ID { get; set; }
+        public string EnglishName { get; set; }
+        public string Name { get; set; }
+        public int TriggerType { get; set; }
+        public string Image { get; set; }
+        public int AvailableRace { get; set; }
+    }
+}
