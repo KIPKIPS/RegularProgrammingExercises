f4d0d08a6ac256e715e7b8c7cf1adb13d697b768
KIPKIPS <2432719656@qq.com>
2021-03-13 02:19:57 +0800

    提交堆化函数

---
+++ Works for 2021/HeapSort/HeapSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HeapSort {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+
+        //堆化,维护堆的性质
+        public void Heapify(int[] tree, int index) {
+            int length = tree.Length;
+            int leftIndex = index * 2 + 1;//左子索引
+            int rightIndex = index * 2 + 2;//右子索引
+            int tempMaxIndex = index;//值最大的索引
+            //查找最大值索引
+            if (leftIndex < length && tree[leftIndex] > tree[tempMaxIndex]) {
+                tempMaxIndex = leftIndex;
+            }
+            if (rightIndex < length && tree[rightIndex] > tree[tempMaxIndex]) {
+                tempMaxIndex = rightIndex;
+            }
+
+            if (tempMaxIndex != index) { //将最大值放到根上
+                Swap(tree, tempMaxIndex, index);
+                Heapify(tree, tempMaxIndex);
+            }
+        }
+
+        void Swap(int[] tree, int a, int b) {
+            int temp = tree[a];
+            tree[a] = tree[b];
+            tree[b] = temp;
+        }
+
+        //建堆 从最后一个非叶子结点开始,自底向上Heapify
+        void BuildHeap(int[] tree)
+        {
+            int lastNode = tree.Length - 1;
+            int parent = (int)MathF.Floor((lastNode - 1) / 2);//查找到最后一个非叶子节点
+        }
+    }
+}
