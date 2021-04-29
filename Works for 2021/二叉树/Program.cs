83b329e7159e693b2fe79a19ed465e2b54633038
KIPKIPS <2432719656@qq.com>
2021-04-29 22:50:44 +0800

    Update Program.cs

--- Works for 2021/二叉树/Program.cs
+++ Works for 2021/二叉树/Program.cs
@@ -1,22 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace 二叉树
-{
-    public class TreeNode
-    {
+namespace 二叉树 {
+    public class TreeNode {
         public string value;
         public TreeNode left;
         public TreeNode right;
-        public TreeNode(string _value)
-        {
+        public TreeNode(string _value) {
             value = _value;
         }
     }
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    class Program {
+        static void Main(string[] args) {
             TreeNode A = new TreeNode("A");
             TreeNode B = new TreeNode("B");
             TreeNode C = new TreeNode("C");
@@ -29,87 +24,64 @@ namespace 二叉树
             B.left = D;
             B.right = E;
             C.left = F;
-            PostOrderTraversal(A);
+            PostOrderTraversalSingleStack(A);
             Console.Read();
         }
         //前序遍历,递归版本
-        static void PreorderTraversalRecursion(TreeNode root)
-        {
+        static void PreorderTraversalRecursion(TreeNode root) {
             Console.Write(root.value + " ");
-            if(root.left != null)
-            {
+            if (root.left != null) {
                 PreorderTraversalRecursion(root.left);
             }
-            if (root.right != null)
-            {
+            if (root.right != null) {
                 PreorderTraversalRecursion(root.right);
             }
         }
         //前序遍历,非递归版本,栈实现
-        static Stack<TreeNode> preorderTraversalStack = new Stack<TreeNode>();
-        static void PreorderTraversal(TreeNode root)
-        {
+        static Stack<TreeNode> traversalStack = new Stack<TreeNode>();
+        static void PreorderTraversal(TreeNode root) {
             TreeNode temp = root;
-            while (true)
-            {
-                if(temp != null)
-                {
+            while (true) {
+                if (temp != null) {
                     Console.Write(temp.value);
-                    preorderTraversalStack.Push(temp);
+                    traversalStack.Push(temp);
                     temp = temp.left;
-                }
-                else
-                {
-                    if(preorderTraversalStack.Count> 0)
-                    {
-                        temp = preorderTraversalStack.Pop().right;
-                    }
-                    else
-
-                    {
+                } else {
+                    if (traversalStack.Count > 0) {
+                        temp = traversalStack.Pop().right;
+                    } else {
                         break;
                     }
                 }
             }
-            
+
         }
-        
-        static void InorderTraversalRecursion(TreeNode root)
-        {
-            
-            if (root.left != null)
-            {
+
+        static void InorderTraversalRecursion(TreeNode root) {
+
+            if (root.left != null) {
                 InorderTraversalRecursion(root.left);
             }
             Console.Write(root.value + " ");
-            if (root.right != null)
-            {
+            if (root.right != null) {
                 InorderTraversalRecursion(root.right);
             }
-            
+
         }
 
         //中序遍历,非递归版本,栈实现
-        static void InorderTraversal(TreeNode root)
-        {
+        static void InorderTraversal(TreeNode root) {
             TreeNode temp = root;
-            while (true)
-            {
-                if (temp != null)
-                {
-                    preorderTraversalStack.Push(temp);
+            while (true) {
+                if (temp != null) {
+                    traversalStack.Push(temp);
                     temp = temp.left;
-                }
-                else
-                {
-                    if (preorderTraversalStack.Count > 0)
-                    {
-                        temp = preorderTraversalStack.Pop();
+                } else {
+                    if (traversalStack.Count > 0) {
+                        temp = traversalStack.Pop();
                         Console.Write(temp.value);
                         temp = temp.right;
-                    }
-                    else
-                    {
+                    } else {
                         break;
                     }
                 }
@@ -117,48 +89,59 @@ namespace 二叉树
         }
 
         //后序遍历,递归版本
-        static void PostOrderTraversalRecursion(TreeNode root)
-        {
-            if (root.left != null)
-            {
+        static void PostOrderTraversalRecursion(TreeNode root) {
+            if (root.left != null) {
                 PostOrderTraversalRecursion(root.left);
             }
-            if (root.right != null)
-            {
+            if (root.right != null) {
                 PostOrderTraversalRecursion(root.right);
             }
             Console.Write(root.value + " ");
         }
 
-        //后序遍历,非递归版本,栈实现
-        static void PostOrderTraversal(TreeNode root) {
+        //后序遍历,非递归版本,双栈实现
+        static void PostOrderTraversalDoubleStack(TreeNode root) {
             TreeNode temp = root;
-            TreeNode lastAccessNode = null;
-            while (true){
+            Stack<string> reverseStack = new Stack<string>();
+            while (true) {
                 if (temp != null) {
-                    if (temp.right==null && temp.left==null)
-                    {
-                        lastAccessNode = temp;
-                        Console.Write(temp.value);
+                    reverseStack.Push(temp.value);
+                    //Console.Write(temp.value + " ");
+                    traversalStack.Push(temp);
+                    temp = temp.right;
+                } else {
+                    if (traversalStack.Count > 0) {
+                        temp = traversalStack.Pop().left;
+                    } else {
+                        break;
                     }
-                    if(temp.right != null && lastAccessNode == temp.right)
-                    {
-                        Console.Write(temp.right.value);
-                    }
-                    
-                    preorderTraversalStack.Push(temp);
-                    temp = temp.left;
-                } else{ 
-                    if (preorderTraversalStack.Count > 0) {
-                        TreeNode peek = preorderTraversalStack.Pop();
-                        temp = peek.right;
-                        //lastAccessNode = temp;
-                        //if (lastAccessNode == temp && temp != null)
-                        //{
-                        //lastAccessNode = temp;
-                        //Console.Write(temp.value);
-                        //}
+                }
+            }
+            while(reverseStack.Count > 0) {
+                Console.Write(reverseStack.Pop() + " ");
+            }
+        }
 
+        //后序遍历,非递归版本,单栈实现
+        static void PostOrderTraversalSingleStack(TreeNode root) {
+            TreeNode temp = root;
+            TreeNode lastAccess = null;//上一次访问的节点
+            while (true) {
+                if (temp != null) {
+                    //Console.Write(temp.value + " ");
+                    traversalStack.Push(temp);
+                    temp = temp.left;
+                } else {
+                    if (traversalStack.Count > 0) {
+                        temp = traversalStack.Pop();
+                        if (temp.right == null || lastAccess == temp.right) { //访问节点的时机,(1)叶节点 (2)上次访问的节点是当前访问节点的右节点
+                            Console.Write(temp.value + " ");//Access
+                            lastAccess = temp;
+                            temp = null;
+                        } else {
+                            traversalStack.Push(temp);
+                            temp = temp.right;
+                        }
                     } else {
                         break;
                     }
