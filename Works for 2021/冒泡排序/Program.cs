34cabb47493e840f767e569e96f19df8903d6ba8
KIPKIPS <2432719656@qq.com>
2021-05-11 20:09:24 +0800

    冒泡排序

---
+++ Works for 2021/冒泡排序/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace 冒泡排序 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            int[] array = { 1, 5, 2, 45, 125, 55, 123, 10, 8 };
+            BubbleSort(array);
+            for(int i = 0;i < array.Length; i++) {
+                Console.Write(array[i] + " ");
+            }
+            Console.ReadLine();
+        }
+        static void BubbleSort(int[] arr) {
+            for (int i = 0; i < arr.Length - 1; i++) {
+                for (int j = 0; j < arr.Length - 1 - i; j++) {
+                    if (arr[j] > arr[j + 1]) {
+                        Swap(arr, j, j + 1);
+                    }
+                }   
+            }
+        }
+        static void Swap(int[] arr,int i,int j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
